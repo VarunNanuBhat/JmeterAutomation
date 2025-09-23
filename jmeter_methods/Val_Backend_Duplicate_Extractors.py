de5fa8c6df7f48775fc94509a14159a9e2ef4179
Varun <bhat.varun.25@gmail.com>
2025-09-23 09:47:55 +0530

    duplicate variable/extractor completed

--- jmeter_methods/Val_Backend_Duplicate_Extractors.py
+++ jmeter_methods/Val_Backend_Duplicate_Extractors.py
@@ -9,22 +9,6 @@ def _get_element_name(element):
     return element.get('testname', 'Unnamed Element')
 
 
-def _create_parent_map(root_element):
-    """Creates a dictionary mapping each child element to its parent."""
-    parent_map = {c: p for p in root_element.iter() for c in p}
-    return parent_map
-
-
-def _get_thread_group_context(element, parent_map):
-    """Traverses up the XML tree to find the name of the parent Thread Group."""
-    ancestor = element
-    while ancestor is not None and ancestor.tag != 'TestPlan':
-        if ancestor.tag in ['ThreadGroup', 'SetupThreadGroup', 'PostThreadGroup', 'TestFragmentController']:
-            return _get_element_name(ancestor)
-        ancestor = parent_map.get(ancestor)
-    return "Global/Unassigned"
-
-
 def analyze_jmeter_script(root_element, enabled_validations):
     """
     Analyzes a JMX script for duplicate variable definitions and redundant extractors.
@@ -34,91 +18,115 @@ def analyze_jmeter_script(root_element, enabled_validations):
     if THIS_VALIDATION_OPTION_NAME not in enabled_validations:
         return module_issues, []
 
-    parent_map = _create_parent_map(root_element)
-
     defined_variables = {}
     extractor_signatures = {}
 
+    # New logic to handle the sibling relationship between controllers and hashTrees
+    last_controller_name = "Global/Unassigned"
+
+    # Iterate through the elements to correctly associate controllers with their hashTrees
     for element in root_element.iter():
+        element_tag = element.tag
         element_name = _get_element_name(element)
-        thread_group_context = _get_thread_group_context(element, parent_map)
 
-        variable_names = []
-        element_type = element.tag
+        # A controller's name is in the same hashTree level as its own <hashTree>
+        if element_tag in ['ThreadGroup', 'SetupThreadGroup', 'PostThreadGroup', 'TestFragmentController']:
+            last_controller_name = element_name
+            continue
 
-        if element.tag == 'Arguments' and element.get('testclass') == 'Arguments':
-            element_type = 'User Defined Variables'
-            for arg in element.findall("./collectionProp[@name='Arguments.arguments']/elementProp"):
-                var_name_prop = arg.find("./stringProp[@name='Argument.name']")
+        # When we find a hashTree, we check its parent to see if it's the main test plan.
+        # This confirms it's a top-level hashTree that contains elements under a sibling controller.
+        is_top_level_element = False
+        try:
+            parent = root_element.findall(".//*")[0].find("..")
+            if parent is None or parent.tag == 'jmeterTestPlan':
+                is_top_level_element = True
+        except IndexError:
+            pass  # The hashTree is likely at the top level
+
+        if element_tag in ['RegexExtractor', 'JSONPostProcessor', 'XPathExtractor', 'CssSelectorExtractor', 'Arguments',
+                           'CSVDataSet', 'CounterConfig', 'RandomVariableConfig']:
+
+            # Use the last known controller's name for context
+            thread_group_context = last_controller_name
+
+            # --- Check for all variable definitions across the script ---
+            variable_names = []
+            element_type = element.tag
+
+            if element.tag == 'Arguments' and element.get('testclass') == 'Arguments':
+                element_type = 'User Defined Variables'
+                for arg in element.findall("./collectionProp[@name='Arguments.arguments']/elementProp"):
+                    var_name_prop = arg.find("./stringProp[@name='Argument.name']")
+                    if var_name_prop is not None and var_name_prop.text:
+                        variable_names.append(var_name_prop.text.strip())
+
+            elif element.tag in ['RegexExtractor', 'JSONPostProcessor', 'XPathExtractor', 'CssSelectorExtractor']:
+                element_type = element.tag
+                var_name_prop = None
+                if element.tag == 'RegexExtractor':
+                    var_name_prop = element.find("./stringProp[@name='RegexExtractor.refname']")
+                elif element.tag == 'JSONPostProcessor':
+                    var_name_prop = element.find("./stringProp[@name='JSONPostProcessor.referenceNames']")
+                elif element.tag == 'XPathExtractor':
+                    var_name_prop = element.find("./stringProp[@name='XPathExtractor.refname']")
+                elif element.tag == 'CssSelectorExtractor':
+                    var_name_prop = element.find("./stringProp[@name='CssSelectorExtractor.refname']")
+
+                if var_name_prop is not None and var_name_prop.text:
+                    for var_name in re.split(';|,', var_name_prop.text):
+                        if var_name:
+                            variable_names.append(var_name.strip())
+
+            elif element.tag == 'CSVDataSet':
+                element_type = 'CSV Data Set Config'
+                var_names_str_prop = element.find("./stringProp[@name='CSVDataSet.variableNames']")
+                if var_names_str_prop is not None and var_names_str_prop.text:
+                    for var_name in var_names_str_prop.text.split(','):
+                        if var_name:
+                            variable_names.append(var_name.strip())
+
+            elif element.tag == 'CounterConfig':
+                element_type = 'Counter'
+                var_name_prop = element.find("./stringProp[@name='CounterConfig.VarName']")
+                if var_name_prop is not None and var_name_prop.text:
+                    variable_names.append(var_name_prop.text.strip())
+            elif element.tag == 'RandomVariableConfig':
+                element_type = 'Random Variable'
+                var_name_prop = element.find("./stringProp[@name='RandomVariableConfig.variableName']")
                 if var_name_prop is not None and var_name_prop.text:
                     variable_names.append(var_name_prop.text.strip())
 
-        elif element.tag in ['RegexExtractor', 'JSONPostProcessor', 'XPathExtractor', 'CssSelectorExtractor']:
-            element_type = element.tag
-            var_name_prop = None
-            if element.tag == 'RegexExtractor':
-                var_name_prop = element.find("./stringProp[@name='RegexExtractor.refname']")
-            elif element.tag == 'JSONPostProcessor':
-                var_name_prop = element.find("./stringProp[@name='JSONPostProcessor.referenceNames']")
-            elif element.tag == 'XPathExtractor':
-                var_name_prop = element.find("./stringProp[@name='XPathExtractor.refname']")
-            elif element.tag == 'CssSelectorExtractor':
-                var_name_prop = element.find("./stringProp[@name='CssSelectorExtractor.refname']")
-
-            if var_name_prop is not None and var_name_prop.text:
-                for var_name in re.split(';|,', var_name_prop.text):
-                    if var_name:
-                        variable_names.append(var_name.strip())
-
-        elif element.tag == 'CSVDataSet':
-            element_type = 'CSV Data Set Config'
-            var_names_str_prop = element.find("./stringProp[@name='CSVDataSet.variableNames']")
-            if var_names_str_prop is not None and var_names_str_prop.text:
-                for var_name in var_names_str_prop.text.split(','):
-                    if var_name:
-                        variable_names.append(var_name.strip())
-
-        elif element.tag == 'CounterConfig':
-            element_type = 'Counter'
-            var_name_prop = element.find("./stringProp[@name='CounterConfig.VarName']")
-            if var_name_prop is not None and var_name_prop.text:
-                variable_names.append(var_name_prop.text.strip())
-        elif element.tag == 'RandomVariableConfig':
-            element_type = 'Random Variable'
-            var_name_prop = element.find("./stringProp[@name='RandomVariableConfig.variableName']")
-            if var_name_prop is not None and var_name_prop.text:
-                variable_names.append(var_name_prop.text.strip())
-
-        for var_name in variable_names:
-            if var_name not in defined_variables:
-                defined_variables[var_name] = []
-            defined_variables[var_name].append({
-                'element_name': element_name,
-                'element_type': element_type,
-                'thread_group': thread_group_context
-            })
-
-        if element.tag in ['RegexExtractor', 'JSONPostProcessor', 'XPathExtractor', 'CssSelectorExtractor']:
-            extractor_path = None
-            if element.tag == 'RegexExtractor':
-                extractor_path = element.findtext("./stringProp[@name='RegexExtractor.regex']")
-            elif element.tag == 'JSONPostProcessor':
-                extractor_path = element.findtext("./stringProp[@name='JSONPostProcessor.jsonPathExpr']")
-            elif element.tag == 'XPathExtractor':
-                extractor_path = element.findtext("./stringProp[@name='XPathExtractor.xpathQuery']")
-            elif element.tag == 'CssSelectorExtractor':
-                extractor_path = element.findtext("./stringProp[@name='CssSelectorExtractor.selector']")
-
-            if extractor_path:
-                parent_element_name = _get_element_name(parent_map.get(element, None))
-                signature = f"{element.tag}_{extractor_path}_{parent_element_name}"
-
-                if signature not in extractor_signatures:
-                    extractor_signatures[signature] = []
-                extractor_signatures[signature].append({
+            for var_name in variable_names:
+                if var_name not in defined_variables:
+                    defined_variables[var_name] = []
+                defined_variables[var_name].append({
                     'element_name': element_name,
+                    'element_type': element_type,
                     'thread_group': thread_group_context
                 })
+
+            # --- Check for duplicate extractors (same path/pattern) ---
+            if element.tag in ['RegexExtractor', 'JSONPostProcessor', 'XPathExtractor', 'CssSelectorExtractor']:
+                extractor_path = None
+                if element.tag == 'RegexExtractor':
+                    extractor_path = element.findtext("./stringProp[@name='RegexExtractor.regex']")
+                elif element.tag == 'JSONPostProcessor':
+                    extractor_path = element.findtext("./stringProp[@name='JSONPostProcessor.jsonPathExpr']")
+                elif element.tag == 'XPathExtractor':
+                    extractor_path = element.findtext("./stringProp[@name='XPathExtractor.xpathQuery']")
+                elif element.tag == 'CssSelectorExtractor':
+                    extractor_path = element.findtext("./stringProp[@name='CssSelectorExtractor.selector']")
+
+                if extractor_path:
+                    signature = f"{element.tag}_{extractor_path}_{last_controller_name}"
+
+                    if signature not in extractor_signatures:
+                        extractor_signatures[signature] = []
+                    extractor_signatures[signature].append({
+                        'element_name': element_name,
+                        'thread_group': thread_group_context
+                    })
 
     # 1. Report duplicate variable name conflicts
     for var_name, sources in defined_variables.items():
